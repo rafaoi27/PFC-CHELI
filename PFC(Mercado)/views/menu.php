11ee85039b1f4bfe32b800d0eee1db5dc0469510
rafaoi27 <60783491+rafaoi27@users.noreply.github.com>
2020-03-05 11:29:49 -0300

    Add files via upload

---
+++ PFC(Mercado)/views/menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/estilo.css">
+    <title>Controle de Mercadinho</title>
+</head>
+
+<body>
+    
+
+            <div class="collapse navbar-collapse" id="nav">
+            <ul>
+                <li ><div class="venda" ><img src="../fotos/venda.png" alt="" srcset=""></div></li>
+                <!-- <li><span class="venda"></span><a href="#">Cadastro de Produtos</a></li>
+                <li><span class="venda"></span><a href="#">Sair do Sistema</a></li>
+           </div> -->
+        </nav>
+    </div>
+</body>
+
+</html>
